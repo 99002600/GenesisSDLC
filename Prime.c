2aa9116838485f87e3290e1553037846a8ec2de9
99002600 <72514189+99002600@users.noreply.github.com>
2020-10-08 21:42:57 +0530

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -34,12 +34,7 @@ long int multiplyNumbers(int n);
    
 }
 
-long int multiplyNumbers(int n) {
-    if (n>=1)
-        return n*multiplyNumbers(n-1);
-    else
-        return 1;
-}
+
 
     return 0;
 }
